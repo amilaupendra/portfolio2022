68a8a54a6e8cf5a25b2e1babad9b4998849fc775
amilaupendra <amilaupendra5@gmailcom>
2024-04-20 23:22:13 +0530

    navbar changed

--- src/Components/navbar/Topnav.jsx
+++ src/Components/navbar/Topnav.jsx
@@ -1,36 +1,28 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
-//import { Link } from "react-router-dom";
-import { HashLink as Link } from 'react-router-hash-link';
 import "./topnav.css";
+
 
 export default function Topnav() {
   return (
-    <div className="headerdiv">
-      <Navbar fixed="top"  bg="dark" variant="dark">
-        <Container >
-          <Navbar.Brand  href="#home"> Amila Upendra</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link>
-              <Link to="#home">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="#about">About</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="#experience">Experience</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="#works">Works</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="#contactus">Contact</Link>
-            </Nav.Link>
+    <Navbar expand="md" className="bg-body-tertiary navigation">
+      <Container fluid>
+        <Navbar.Brand  href="#home" style={{color:'white'}}>Amila Upendra</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#works">Works</Nav.Link>
+            <Nav.Link href="#contactus">Contact</Nav.Link>
           </Nav>
-          </Container>
-
-      </Navbar>
-      
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
